b5ddf7c833da1004e0cfeea9cd5e8e0792852728
Yauheni Bandyukevich <zhenya.bandyukevich@mail.ru>
2020-05-27 23:58:30 +0300

    feat: add menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+const swiperPaginations = document.querySelector('.swiper-pagination');
+const headerMenuBtn = document.querySelector('.header-menu-btn');
+const menu = document.querySelector('.menu');
+const closeButton = document.querySelector('.close-button');
+
+headerMenuBtn.addEventListener('click', () => {
+    menu.classList.add('active');
+    swiperPaginations.style.display = 'none';
+});
+
+closeButton.addEventListener('click', () => {
+    menu.classList.remove('active');
+    swiperPaginations.style.display = 'block';
+});
+
 const swiper = new Swiper('.swiper-container', {
     pagination: {
         el: '.swiper-pagination',
